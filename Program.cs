1dcde41e0a3567a6fd06ae3ff6a3af4256db49a1
Andersonp-Silva25 <andersonp.silva25@gmail.com>
2023-12-03 19:50:05 -0300

    :sparkles:Feat: Implementando a classe Iphone

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 Nokia nokia = new Nokia("123456", "Nokia", "asd-123", 64);
+Console.WriteLine("Nokia");
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Iphone iphone = new Iphone("654321", "Iphone", "dsa-321", 128);
+Console.WriteLine("Iphone");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
